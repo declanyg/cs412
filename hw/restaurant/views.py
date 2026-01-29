3bd24500c6da2eb69e8224244824cc8406a0d9d2
declanyg <declan@youngs.asia>
2026-01-29 17:22:03 -0500

    Adding assignment 2 completion and deployment

--- hw/restaurant/views.py
+++ hw/restaurant/views.py
@@ -4,7 +4,30 @@ import time
 import datetime
 
 # Create your views here.
-from django.http import HttpResponse
+daily_specials = [
+        "Banana Split $3.00",
+        "Banana Pancake $2.75",
+        "Banana Oatmeal Cookie $2.50",
+        "Banana Muffin $2.25",
+        "Fried Bananas $2.00",
+        "Banana Pudding $3.50",
+        "Banana Foster $4.00"
+    ]
+prices = {
+    'Banana': 1.00,
+    'Peeled Banana': 1.50,
+    'Banana Smoothie': 2.00,
+    'Banana Bread': 2.50,
+    'Banana Cream Pie': 5.00,
+    #Specials
+    'Banana Split': 3.00,
+    'Banana Pancake': 2.75,
+    'Banana Oatmeal Cookie': 2.50,
+    'Banana Muffin': 2.25,
+    'Fried Bananas': 2.00,
+    'Banana Pudding': 3.50,
+    'Banana Foster': 4.00
+}
 
 def main(request):
 
@@ -17,26 +40,43 @@ def main(request):
 
 def order(request):
     weekday = datetime.datetime.today().weekday()
-    daily_specials = [
-        "Banana Split $3.00",
-        "Banana Pancake $2.75",
-        "Banana Oatmeal Cookie $2.50",
-        "Banana Muffin $2.25",
-        "Fried Bananas $2.00",
-        "Banana Pudding $3.50",
-        "Banana Foster $4.00"
-    ]
 
     context = {
-        "daily_special": daily_specials[weekday],
         "generated_time": time.ctime()
     }
 
     return render(request, "restaurant/order.html", context)
 
 def confirmation(request):
-    context = {
-        "generated_time": time.ctime()
-    }
+    weekday = datetime.datetime.today().weekday()
+    weekly_special = daily_specials[weekday].split(" $")[0]
+    total = 0.0
 
-    return render(request, "restaurant/confirmation.html", context)
+    context = {}
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        phone = request.POST.get('phone', '')
+        email = request.POST.get('email', '')
+        special_instructions = request.POST.get('special instruction', '')
+
+        items = []
+        for item_name in ['Banana', 'Peeled Banana', 'Banana Smoothie', 'Banana Bread', 'Banana Cream Pie']:
+            if request.POST.get(item_name):
+                items.append(f"{item_name} ${prices[item_name]:.3f}")
+                total += prices[item_name]
+        if request.POST.get('Daily Special'):
+            items.append(f"{weekly_special} ${prices[weekly_special]:.3f}")
+            total += prices[weekly_special]
+
+        context = {
+            'name': name,
+            'phone': phone,
+            'email': email,
+            'special_instructions': special_instructions,
+            'items': items,
+            'total': total,
+            "generated_time": time.ctime()
+        }
+
+    return render(request, 'restaurant/confirmation.html', context)
