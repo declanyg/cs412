2d8e6d6a90c278d1e337c20b8d9e2988bb756f6a
declanyg <declan@youngs.asia>
2026-03-20 00:41:23 -0400

    Fixed load data

--- hw/voter_analytics/models.py
+++ hw/voter_analytics/models.py
@@ -1,6 +1,11 @@
+# File: models.py
+# Author: Declan Young (declanyg@bu.edu), 3/19/2026
+# Description: models file for voter_analytics app
+
 import csv
 import datetime
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Voter(models.Model):
@@ -21,30 +26,35 @@ class Voter(models.Model):
     def load_data(cls, file_path):
         with open(file_path, newline='') as f:
             reader = csv.reader(f)
+            next(reader)
 
             for row in reader:
-                voter = cls.objects.create(
-                    last_name=row[1],
-                    first_name=row[2],
-                    date_of_birth=datetime.strptime(row[7], "%m/%d/%Y").date(),
-                    registration_date=datetime.strptime(row[8], "%m/%d/%Y").date(),
-                    party_affiliation=row[9],
-                    precinct_number=row[10],
+                try:
+                    voter = cls.objects.create(
+                        last_name=row[1],
+                        first_name=row[2],
+                        date_of_birth=date.fromisoformat(row[7]),
+                        registration_date=date.fromisoformat(row[8]),
+                        party_affiliation=row[9],
+                        precinct_number=row[10],
 
-                    v20state=row[11],
-                    v21town=row[12],
-                    v21primary=row[13],
-                    v22general=row[14],
-                    v23town=row[15],
-                )
+                        v20state=row[11],
+                        v21town=row[12],
+                        v21primary=row[13],
+                        v22general=row[14],
+                        v23town=row[15],
+                    )
 
-                Residential_Address.objects.create(
-                    voter=voter,
-                    street_number=row[3],
-                    street_name=row[4],
-                    apartment_number=row[5],
-                    zip_code=row[6],
-                )
+                    Residential_Address.objects.create(
+                        voter=voter,
+                        street_number=row[3],
+                        street_name=row[4],
+                        apartment_number=row[5],
+                        zip_code=row[6],
+                    )
+                except ValueError as e:
+                    print(f"Error occurred while processing row: {row}")
+                    print(f"Error message: {e}")
 
 class Residential_Address(models.Model):
     voter = models.ForeignKey(Voter, on_delete=models.CASCADE, related_name='residential_addresses')
